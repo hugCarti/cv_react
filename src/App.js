7c9310bcdf78113f72efbd120fbdc672d7dab78b
pingouin <hleplingard04@gmail.com>
2024-10-08 16:44:47 +0200

    g

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
 import React from "react";
-import Nav from './container/nav.jsx'
+import { Routes, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
+import Accueil from "./container/accueil.jsx";
+import Services from "./container/services.jsx";
+import Réalisations from "./container/réalisations.jsx";
+import Blog from "./container/blog.jsx";
+import Mecontacter from "./container/mecontacter.jsx";
 
 export default function App() {
   return (
     <div className="App">
-      <header>
-        < Nav />
-      </header>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">ACCUEIL</Link>
+            </li>
+            <li>
+              <Link to="/services">SERVICES</Link>
+            </li>
+            <li>
+              <Link to="/realisations">REALISATIONS</Link>
+            </li>
+            <li>
+              <Link to="/blog">BLOG</Link>
+            </li>
+            <li>
+              <Link to="/mecontacter">ME CONTACTER</Link>
+            </li>
+          </ul>
+        </nav>
 
+        <Routes>
+          <Route path='/' element={<Accueil />} />
+          <Route path='/services' element={<Services />} />
+          <Route path='/realisations' element={<Réalisations />} />
+          <Route path='/blog' element={<Blog />} />
+          <Route path='/mecontacter' element={<Mecontacter />} />
+        </Routes>
     </div>
   );
 }
